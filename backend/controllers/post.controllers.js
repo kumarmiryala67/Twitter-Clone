c8d5f73857e8134cb31f17381722ce91c46fbc89
kumarmiryala67 <rajkumarmiriyala1996@gmail.com>
2024-10-28 11:36:48 +0530

    3-like post mutation added

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -4,60 +4,87 @@ import Post from "../models/Post.models.js";
 import {v2 as cloudinary } from 'cloudinary';
 
 export const createPost = async (req,res) => {
+
     try {
          const { text} = req.body;
          let { img } = req.body;
          const userId = req.user._id.toString();
 
-         const user = await User.findById(userId)
-         if(!user) return res.status(404).json({message:"User not found"})
+         const user = await User.findById(userId);
+         if(!user) return res.status(404).json({message:"User not found"});
         
         if(!text && !img) {
-            return res.status(400).json({error: "Post must have text or image "})
+            return res.status(400).json({error: "Post must have text or image "});
         }
 
         if(img) {
-            const uploadedResponse = await cloudinary.uploader.upload(img)
+            const uploadedResponse = await cloudinary.uploader.upload(img);
             img = uploadedResponse.secure_url;
         }
 
         const newPost= new Post ({
             user:userId,
             text,
-            img
-        })
+            img,
+        });
 
         await newPost.save();
         res.status(201).json(newPost);
     } catch (error) { 
         
-    } res.status(500).json({ error:"Internal server error" });
+    res.status(500).json({ error:"Internal server error" });
     console.log("Error in createPost controller: ", error );
+}
 };
 
 export const deletePost = async (req,res) => {
     try {
-        const post = await Post.findById(req.params.id)
-        if(!post) {
-            return res.status(404).json({error: "Post not found"})
-        }
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-        if(post.user.toString()  !== req.user._id.toString()) {
-            return res.status(401).json({error: "You are not authorized to delete this post"})
-        }
+		if (post.user.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "You are not authorized to delete this post" });
+		}
 
-        if(post.img) {
-            const imgId = post.img.split("/").pop().split(".")[0];
-            await cloudinary.uploader.destroy(imgId);
-        }
-        
-        await Post.findByIdAndDelete(req.params.id);
-        res.status (200).json({message: "Post deleted successfully"});
-    } catch (error) {
-       console.log("Error in deletePost controller: ", error);
-       res.status(500).json({error: "Internal server error"});
-    }
+		if (post.img) {
+			const imgId = post.img.split("/").pop().split(".")[0];
+			await cloudinary.uploader.destroy(imgId);
+		}
+
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (error) {
+		console.log("Error in deletePost controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 };
+
+//     try {
+//         const post = await Post.findById(req.params.id)
+//         if(!post) {
+//             return res.status(404).json({error: "Post not found"})
+//         }
+
+//         if(post.user.toString()  !== req.user._id.toString()) {
+//             return res.status(401).json({error: "You are not authorized to delete this post"})
+//         }
+
+//         if(post.img) {
+//             const imgId = post.img.split("/").pop().split(".")[0];
+//             await cloudinary.uploader.destroy(imgId);
+//         }
+        
+//         await Post.findByIdAndDelete(req.params.id);
+
+//         res.status (200).json({message: "Post deleted successfully"});
+//     } catch (error) {
+//        console.log("Error in deletePost controller: ", error);
+//        res.status(400).json({error: "Internal server error"});
+//     }
+// };
 
 export const commentOnPost = async (req, res) => {
   try {
@@ -102,7 +129,10 @@ export const likeUnlikePost = async (req,res) => {
             // Unlike Post
             await Post.updateOne({_id:postId}, {$pull: {likes: userId}})
             await User.updateOne({_id: userId }, { $pull: { likedPosts: postId }});
-            res.status(200).json({message: "Post unliked successfully"})
+ 
+            const updatedLikes = post.likes.filter((id) => id.toString() !==userId.toString
+        ());
+            res.status(200).json(updatedLikes);
         } else {
             // Like post  
             post.likes.push(userId);
@@ -116,7 +146,8 @@ export const likeUnlikePost = async (req,res) => {
             });
             await notification.save();
 
-            res.status(200).json({ message: "Post liked successfully"});
+            const updatedLikes = post.likes;
+            res.status(200).json(updatedLikes);
         }
     } catch (error) {
         console.log("Error in likeUnlikePost controller: error");
