872f2532d6309be318a39cbeb36321be7853eb72
kumarmiryala67 <rajkumarmiriyala1996@gmail.com>
2024-10-30 13:06:15 +0530

    changed youtubelink

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
 
     profileImg:{
         type: String,
-        default: "",
+        default: "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=",
     },
     coverImg:{
          type: String,
